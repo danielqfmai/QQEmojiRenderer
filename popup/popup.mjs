ae073b3ad603a989eb2baa163b2cc493c73ccb0d
danielqfmai <13928009585@163.com>
2024-08-01 15:31:35 +0800

    Init.

---
+++ popup/popup.mjs
@@ -0,0 +1,16 @@
+import * as emoji from "../scripts/emoji.mjs"
+
+const grid = document.querySelector('#img_grid');
+
+emoji.keys.forEach(key => {
+	const div = document.createElement('div');
+	div.classList.add('flexible');
+	div.title = '/' + key;
+	const img = document.createElement('img');
+	img.src = emoji.getURL(key);
+	div.appendChild(img);
+//	const p = document.createElement('p');
+//	p.innerText = '/' + key;
+//	div.appendChild(p);
+	grid.appendChild(div);
+});
